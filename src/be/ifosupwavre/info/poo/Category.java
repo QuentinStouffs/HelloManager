877261b61879465d22dd800f2a85f61bbfb221b0
quentinstouffs <quentin.st@gmail.com>
2019-09-18 20:53:14 +0200

    Category Enum

---
+++ src/be/ifosupwavre/info/poo/Category.java
@@ -0,0 +1,18 @@
+package be.ifosupwavre.info.poo;
+
+
+/**
+ * Enum
+ */
+public enum Category {
+    MEAT("Viande"),
+    POULTRY("Volaille"),
+    FISH("Poisson"),
+    VEGGIE("Végé")
+
+    /**
+     * Empty constructor
+     * @param cat Text
+     */
+    Category(String cat) {}
+}
